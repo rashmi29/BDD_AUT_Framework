52fc7ab16ce45f72dce3739e05993cbc8f92a24d
Rashmi Patel <rashmi5904@gmail.com>
2023-06-04 15:25:15 -0700

    Added test runner file

--- BDD_Automation_Framework/src/test/java/org/qa/testrunner/TestRunner.java
+++ BDD_Automation_Framework/src/test/java/org/qa/testrunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = { "src/test/resources/Features" },
+		features = { "src/test/resources/org/qa/features" },
 		glue = { "stepdefinition", "apphooks" },
 		// tags = "@Smoke",
 		plugin = { "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }
